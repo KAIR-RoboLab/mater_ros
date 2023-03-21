a23fbf29cbd3444bed223be0574b59578a6af5d3
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2023-03-21 11:26:53 +0100

    Run black on rviz launch

--- mater_bringup/launch/rviz2.launch.py
+++ mater_bringup/launch/rviz2.launch.py
@@ -7,16 +7,10 @@ import os
 
 def generate_launch_description():
     mater_bringup = get_package_share_directory("mater_bringup")
-    
-    rviz_config = PathJoinSubstitution([mater_bringup, "rviz", "mater.rviz"])
-    rviz2_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=["-d", rviz_config]
-    )
 
-    actions = [
-        rviz2_node
-    ]
+    rviz_config = PathJoinSubstitution([mater_bringup, "rviz", "mater.rviz"])
+    rviz2_node = Node(package="rviz2", executable="rviz2", arguments=["-d", rviz_config])
+
+    actions = [rviz2_node]
 
     return LaunchDescription(actions)
